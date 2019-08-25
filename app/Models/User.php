349839dcc5121c1a2ab334ccebedf07907a00ba6
mars <511391805@qq.com>
2019-08-25 14:15:35 +0800

    微博数据模型

--- app/Models/User.php
+++ app/Models/User.php
@@ -36,16 +36,23 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
     public function gravatar($size = '100')
     {
         $hash = md5(strtolower(trim($this->attributes['email'])));
         return "http://www.gravatar.com/avatar/$hash?s=$size";
     }
+
     public static function boot()
     {
         parent::boot();
         static::creating(function ($user) {
             $user->activation_token = str_random(10);
         });
+    }
+
+    public function statuses()
+    {
+        return $this->hasMany(Status::class);
     }
 }
